3fba199d3bd9a47174f396b30a5421df21e88c15
robertlovin23 <robert.a.lovin@gmail.com>
2020-07-02 16:48:11 -0500

    package commit

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ if(process.env.NODE_ENV === 'production'){
     // if it doesn't recognizes the route
     const path = require('path');
 
-    app.get('*', ( req,res ) => {
+    app.get('*', (req,res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
