5109b62c8a4a3e0c4457c4a27330c1a475544ad8
robertlovin23 <robert.a.lovin@gmail.com>
2020-07-15 16:23:21 -0500

    new commit

--- client/src/components/pages/DeleteItem.js
+++ client/src/components/pages/DeleteItem.js
@@ -7,43 +7,13 @@ import {connect} from 'react-redux'
 import {fetchItem,deleteItem} from '../../actions'
 
 class DeleteItem extends React.Component{
-
-    componentDidMount(){
-        this.props.fetchItem(this.props.match.params.id);
-    }
-
-    // renderModalButtons = () => {
-    //     return(
-    //         <React.Fragment>
-    //             <Button color="primary" onClick={() => this.deleteItem(this.props.match.params.id)}>Delete Item</Button>
-    //             <Link to={"/"} className="waves-effect waves-light btn">Cancel</Link>
-    //         </React.Fragment>
-    //     )
+    
+    // componentDidMount(){
+    //     this.props.fetchItem(this.props.match.params.id);
     // }
     render(){
-
         return(
-                <Dialog
-                        open={this.props.openModal}
-                        onClose={this.props.handleClose}
-                        aria-labelledby="alert-dialog-title"
-                        aria-describedby="alert-dialog-description"
-                    >
-                        <DialogTitle id="alert-dialog-title">{"Delete"}</DialogTitle>
-                        <DialogContent>
-                            <DialogContentText id="alert-dialog-description">
-                                Would you like to delete this item?
-                            </DialogContentText>
-                        </DialogContent>
-                        <DialogActions>
-                            <Button onClick={this.props.handleClose} color="primary">
-                                Go Back
-                            </Button>
-                            <Button onClick={() => this.props.deleteItem(this.props.match.params.id)} color="primary" autoFocus>
-                                Delete
-                            </Button>
-                        </DialogActions>
-                    </Dialog>
+            <div></div>
         )
     }
 }
